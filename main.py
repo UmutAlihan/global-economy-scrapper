15ee285e5192c276cf880d17a1a42789f0ba21a2
Umut Alihan Dikel <umut_alihan.dikel@daimler.com>
2019-08-22 21:34:19 +0300

    get data from csv download
    I am downloading all data using "Download" button link on site
    
    It returns countries all year & all indicator data
    
    Co-Authored-By: kavakhalil <kavakhalil@users.noreply.github.com>
    Co-Authored-By: emredmmr <emredmmr@users.noreply.github.com>
    Co-Authored-By: Atra Bahçeci <atrabahceci@users.noreply.github.com>

--- main.py
+++ main.py
@@ -2,42 +2,44 @@ from html5print import HTMLBeautifier as htmlb
 import re
 import csv
 import urllib.request
+import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 import os, ssl
 if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
     getattr(ssl, '_create_unverified_context', None)): 
     ssl._create_default_https_context = ssl._create_unverified_context
-atra = open('trial.csv', 'w', newline = '')
-writer = csv.writer(atra)
-soup = BeautifulSoup(urllib.request.urlopen("https://wits.worldbank.org/countrysnapshot/en/TUR").read(), 'lxml')
-
-tbody = soup('table', {"class":"alignRight data"})[0].find_all('tr')
-for row in tbody:
-    cols = row.findChildren(recursive=False)
-    cols = [ele.text.strip() for ele in cols]
-    writer.writerow(cols)
-    print(cols)
 
 	
+
+#Country - All Years - All Indicators
+url = "http://wits.worldbank.org/data/public/cp/en_USA_allyears_wits_trade_summary.csv"
+response = requests.get(url, verify=False)
+
+alihan = requests.get(url, verify=False)
+with open("C:\Users\umdikel\Desktop\local\Alihan\global-economy-scrapper\test.csv", "wb") as f:
+	f.write(alihan.content)
+	
+
+
+	
+
 #TR : 2016	
 #https://wits.worldbank.org/CountryProfile/en/Country/TUR/Year/2016/Summary	
-	
-	
 
-"""def compare(row):
-	left = soup('td', {"class":"alignLeft data"})[row]
-	right = soup('td', {"class":"alignRight data"})[row]
-	return(left,right)"""
-	
 
-"""left = soup('td', {"class":"alignLeft data"})
-right = soup('td', {"class":"alignRight data"})"""
-	
-	
-"""table_striped = soup('table', {"class":"table table-striped"})
-
-gdp = re.compile("GDP")
-
-for item in table_striped:
-	item.find_all("GDP")"""
-
+"""def download_file(url, filename):
+	''' Downloads file from the url and save it as filename '''
+	# check if file already exists
+	if not os.path.isfile(filename):
+		print('Downloading File')
+		response = requests.get(url,verify=False)
+		# Check if the response is ok (200)
+		if response.status_code == 200:
+			# Open file and write the content
+			with open(filename, 'wb') as file:
+			# A chunk of 128 bytes
+				for chunk in response:
+					file.write(chunk)
+		else:
+			print('File exists')""""	
